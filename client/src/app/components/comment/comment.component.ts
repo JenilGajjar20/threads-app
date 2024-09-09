37fafdf90f0d40601279ef84f4b8e6d127dc4440
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-09-09 19:01:38 +0530

    WIP

--- client/src/app/components/comment/comment.component.ts
+++ client/src/app/components/comment/comment.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { NgIf } from '@angular/common';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
+import { Comment } from '../../interface/comment.interface';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [ButtonComponent, NgIf],
+  imports: [ButtonComponent, NgIf, CommentFormComponent],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
 })
 export class CommentComponent {
+  @Input() comment!: Comment;
+
   isExpanded = false;
   isReplying = false;
 
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,27 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommentComponent } from '../components/comment/comment.component';
+import { CommentService } from '../services/comment.service';
+import { Comment } from '../interface/comment.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CommentComponent],
+  imports: [CommonModule, CommentComponent, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  commentService = inject(CommentService);
+  comments = signal<Comment[]>([]);
+
+  ngOnInit(): void {
+    this.getComments();
+  }
+
+  getComments() {
+    this.commentService.getComments().subscribe((comments) => {
+      this.comments.set(comments);
+    });
+  }
+}
